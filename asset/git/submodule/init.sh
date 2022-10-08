9a21484db31a76ea7aa47ec7ef46499d97e128e2
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-10-08 23:36:52 +0900

    Fix: asset > git > submodule > init.sh | init&update --> update --init --recursive

--- asset/git/submodule/init.sh
+++ asset/git/submodule/init.sh
@@ -3,6 +3,7 @@
 BRANCH=${1:-2022}
 
 # Init git submodule
-git submodule init
-git submodule update
+# git submodule init
+# git submodule update
+git submodule update --init --recursive
 git submodule foreach git checkout $BRANCH
